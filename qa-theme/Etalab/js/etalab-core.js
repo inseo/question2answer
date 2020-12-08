4d7fdeddcad3c552a34f6210cd196537fadfdde1
Sébastien Delorme <sdelorme@eco-h.fr>
2020-12-08 12:46:47 +0100

    fix(header): a11y: opening/closing management of account and burger toggles

--- qa-theme/Etalab/js/etalab-core.js
+++ qa-theme/Etalab/js/etalab-core.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
 	 */
 	$('#qam-account-toggle').click(function (e) {
 		e.stopPropagation();
+		closeMenuToggle();
 		if ($(this).attr("aria-expanded") == "false")
 			$(this).attr("aria-expanded", "true");
 		else
@@ -28,26 +29,47 @@ $(document).ready(function () {
 		$('.qam-account-items').slideToggle(100);
 	});
 
-	$(document).click(function () {
-		$('#qam-account-toggle.account-active').attr("aria-expanded", "false");
-		$('#qam-account-toggle.account-active').removeClass('account-active');
-		$('.qam-account-items:visible').slideUp(100);
-	});
-
-	$('.qam-account-items').click(function (event) {
-		event.stopPropagation();
-	});
-
 	/**
 	 * Main navigation toggle script
 	 */
-	$('.qam-menu-toggle').click(function () {
+	$('.qam-menu-toggle').click(function (e) {
+		e.stopPropagation();
+		closeAccountToggle();
+
 		$('.qa-nav-main').slideToggle(100);
 		$(this).toggleClass('current');
 		if ($(this).attr("aria-expanded") == "false")
 			$(this).attr("aria-expanded", "true");
 		else
 			$(this).attr("aria-expanded", "false");
+	});
+
+	/**
+	 * Close menu(s) when user click anywhere else
+	 */
+	$(document).click(function () {
+		closeMenuToggle();
+		closeAccountToggle();
+	});
+
+	function closeAccountToggle() {
+		$('#qam-account-toggle.account-active').attr("aria-expanded", "false");
+		$('#qam-account-toggle.account-active').removeClass('account-active');
+		$('.qam-account-items:visible').slideUp(100);
+	}
+
+	function closeMenuToggle() {
+		$('.qam-menu-toggle.current').attr("aria-expanded", "false");
+		$('.qam-menu-toggle.current').removeClass('current');
+		$('.qa-nav-main:visible').slideUp(100);
+	}
+
+	$('.qam-account-items').click(function (event) {
+		event.stopPropagation();
+	});
+
+	$('.qa-nav-main').click(function (event) {
+		event.stopPropagation();
 	});
 
 	/*
