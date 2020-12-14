6f8ef02d23fb10bf7fab65321cd1fb972b8b29c3
Corinne Durrmeyer <corinne@inseo.fr>
2020-12-14 14:18:11 +0100

    feat(account-login): Add styles & update pattern of account-login block

--- qa-theme/Etalab/qa-theme.php
+++ qa-theme/Etalab/qa-theme.php
@@ -118,15 +118,15 @@ class qa_html_theme extends qa_html_theme_base
 				$emailOnly = qa_opt('allow_login_email_only');
 				$inputType = $emailOnly ? 'email' : 'text';
 				$this->output(
-					'<form action="' . $login['url'] . '" method="post">',
+					'<form class="qam-account-login" action="' . $login['url'] . '" method="post">',
 					'<label for="qam-emailhandle">' . trim(qa_lang_html($emailOnly ? 'users/email_label' : 'users/email_handle_label'), ':') . '</label>',
 					'<input type="' . $inputType . '" autocomplete="username" name="emailhandle" id="qam-emailhandle" aria-required="true" dir="auto" />',
 					'<label for="qam-password">' . trim(qa_lang_html('users/password_label'), ':') . '</label>',
 					'<input type="password" autocomplete="current-password" name="password" id="qam-password" aria-required="true" dir="auto" />',
-					'<div><input type="checkbox" name="remember" id="qam-rememberme" value="1"/>',
+					'<div class="qam-checkbox"><input type="checkbox" name="remember" id="qam-rememberme" value="1"/>',
 					'<label for="qam-rememberme">' . qa_lang_html('users/remember_label') . '</label></div>',
 					'<input type="hidden" name="code" value="' . qa_html(qa_get_form_security_code('login')) . '"/>',
-					'<input type="submit" value="' . $login['label'] . '" class="qa-form-tall-button qa-form-tall-button-login" name="dologin"/>',
+					'<input type="submit" value="' . $login['label'] . '" class="qam-button" name="dologin"/>',
 					'</form>'
 				);
 				// remove regular navigation link to log in page
